6cc801e92f6834843348905f3da91cb9d467a5d9
Luca <lucafahrni@yahoo.com>
2021-01-14 13:37:44 +0100

    new project added

---
+++ app/src/main/java/zli/ch/lf/testplants/MainActivity.java
@@ -0,0 +1,18 @@
+package zli.ch.lf.testplants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+ * @author Luca Fahrni
+ * @version 14.01.2021
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
